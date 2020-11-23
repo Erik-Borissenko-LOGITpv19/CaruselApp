f6d06e38011ddd2564c3ab8d162b1505c4a14744
opilane <opilane@HP-CZC6297B8Z>
2020-11-23 17:28:12 +0200

    CarouselPage

--- CaruselApp/CaruselApp/MainPage.xaml.cs
+++ CaruselApp/CaruselApp/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace CaruselApp
@@ -11,6 +12,8 @@ namespace CaruselApp
     public partial class MainPage : CarouselPage
     {
         Image img;
+        Button btn1, btn2, btn3, btn4, btn5;
+        Uri uri;
         public MainPage()
         {
             //-----------------------------------------------------------------------------------------------------------------------------------------------
@@ -46,10 +49,20 @@ namespace CaruselApp
                 }
             };
             //-----------------------------------------------------------------------------------------------------------------------------------------------------
+            btn1 = new Button
+            {
+                BackgroundColor = Color.LightSkyBlue,
+                Text = "Нажми, чтобы узнать подробности :)",
+                TextColor = Color.Black,
+                HorizontalOptions = LayoutOptions.Center,
+            };
+            btn1.Clicked += Btn1_Clicked;
             var yellowContentPage = new ContentPage
             {
+
                 Content = new StackLayout
                 {
+
                         Children =
                         {
                         new Image
@@ -65,19 +78,19 @@ namespace CaruselApp
                             FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
                             HorizontalOptions = LayoutOptions.StartAndExpand
                         },
-                        new Button
-                        {
-                            BackgroundColor = Color.LightSkyBlue,
-                            Text = "Нажми, чтобы узнать подробности :)",
-                            TextColor = Color.Black,
-                            HorizontalOptions = LayoutOptions.Center,
-                        }
-                        
-                        
-                    }
-                }
+        btn1
+                    }            
+        }
             };
             //-----------------------------------------------------------------------------------------------------------------------------------------------------
+            btn2 = new Button
+            {
+                BackgroundColor = Color.LightSkyBlue,
+                Text = "Кликай скорее!:)",
+                TextColor = Color.Black,
+                HorizontalOptions = LayoutOptions.Center,
+            };
+            btn2.Clicked += Btn2_Clicked;
             var greenContentPage = new ContentPage
             {
                 Content = new StackLayout
@@ -97,17 +110,19 @@ namespace CaruselApp
                             FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
                             HorizontalOptions = LayoutOptions.StartAndExpand
                         },
-                        new Button
-                        {
-                            BackgroundColor = Color.LightSkyBlue,
-                            Text = "Нажимай скорее!",
-                            TextColor = Color.Black,
-                            HorizontalOptions = LayoutOptions.Center,
-                        }
+                        btn2
                     }
                 }
             };
             //--------------------------------------------------------------------------------------------------------------------------------------------
+            btn3 = new Button
+            {
+                BackgroundColor = Color.LightSkyBlue,
+                Text = "Информация для тебя :)",
+                TextColor = Color.Black,
+                HorizontalOptions = LayoutOptions.Center,
+            };
+            btn3.Clicked += Btn3_Clicked;
             var blackContentPage = new ContentPage
             {
                 Content = new StackLayout
@@ -127,17 +142,19 @@ namespace CaruselApp
                             FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
                             HorizontalOptions = LayoutOptions.StartAndExpand
                         },
-                        new Button
-                        {
-                            BackgroundColor = Color.LightSkyBlue,
-                            Text = "Информация для тебя :)",
-                            TextColor = Color.Black,
-                            HorizontalOptions = LayoutOptions.Center,
-                        }
+                        btn3
                     }
                 }
             };
             //----------------------------------------------------------------------------------------------------------------------------------------------
+            btn4 = new Button
+            {
+                BackgroundColor = Color.LightSkyBlue,
+                Text = "Кликай скорее!:)",
+                TextColor = Color.Black,
+                HorizontalOptions = LayoutOptions.Center,
+            };
+            btn4.Clicked += Btn4_Clicked;
             var orangeContentPage = new ContentPage
             {
                 Content = new StackLayout
@@ -157,25 +174,75 @@ namespace CaruselApp
                             FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
                             HorizontalOptions = LayoutOptions.StartAndExpand
                         },
-                        new Button
-                        {
-                            BackgroundColor = Color.LightSkyBlue,
-                            Text = "Кликай скорее!:)",
-                            TextColor = Color.Black,
-                            HorizontalOptions = LayoutOptions.Center,
-                        }
+                        btn4
                     }
                 }
             };
             //----------------------------------------------------------------------------------------------------------------------------------------------
-            Children.Add(glavnajaContentPage);
+            btn5 = new Button
+            {
+                BackgroundColor = Color.LightSkyBlue,
+                Text = "Тест на темперамент",
+                TextColor = Color.Black,
+                HorizontalOptions = LayoutOptions.Center,
+            };
+            btn5.Clicked += Btn5_Clicked;
+            var ssilkaContentPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                        {
+                        new Image
+                        {
+                            Source = "maxresdefault.jpg",
+                            HorizontalOptions = LayoutOptions.FillAndExpand,
+                        },
+
+                        new Label
+                        {
+                            Text = "Пройди тест на темперамент!",//Text
+                            TextColor = Color.Black,
+                            FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
+                            HorizontalOptions = LayoutOptions.StartAndExpand
+                        },
+                        btn5
+                    }
+                }
+            };
+        //----------------------------------------------------------------------------------------------------------------------------------------------
+        Children.Add(glavnajaContentPage);
             Children.Add(yellowContentPage);
             Children.Add(greenContentPage);
             Children.Add(blackContentPage);
             Children.Add(orangeContentPage);
+            Children.Add(ssilkaContentPage);
+        }
 
+        private async void Btn5_Clicked(object sender, EventArgs e)
+        {
+            Uri uri = new Uri("https://testometrika.com/personality-and-temper/the-formula-of-temperament-a-belov/");
+            await Browser.OpenAsync(uri);
+        }
 
+        private void Btn4_Clicked(object sender, EventArgs e)
+        {
+            btn4.Text = @"Меланхо́лик (др.-греч. μελαγχολικός «одержимый меланхолией», от μελαγχολία «излияние чёрной жёлчи», ← μέλας «чёрный; тёмный» + χολή «жёлчь; гнев»; буквально: «чёрная жёлчь») — один из четырёх типов темперамента в классификации Гиппократа, характеризующийся инертностью нервной системы, слабостью процессов возбуждения и торможения при их неуравновешенности (характерно преобладание процессов торможения), повышенной чувствительностью и высокой реактивностью психики.";
+        }
 
+        private void Btn3_Clicked(object sender, EventArgs e)
+        {
+            btn3.Text = @"Флегматик (греч. φλέγμα [флегма] — мокрота, слизь) — один из четырёх типов темперамента в классификации Гиппократа. ";
+        }
+
+        private void Btn2_Clicked(object sender, EventArgs e)
+        {
+            btn2.Text = @"Сангвиник (лат. sanguis [сангвис], «кровь», «жизненная сила») — один из четырёх типов темперамента в классификации Гиппократа (преобладание крови).\nЧеловека сангвинического темперамента можно охарактеризовать как живого, подвижного,\nсравнительно легко переживающего неудачи и неприятности.\nТакой темперамент имели Александр Иванович Герцен, австрийский композитор Вольфганг Амадей Моцарт, а также Наполеон Бонапарт";
+        }
+
+        private void Btn1_Clicked(object sender, EventArgs e)
+        {
+            btn1.Text = @"Темпераментный, неуравновешенный человек, легко возбуждающийся\nпод действием каких-н. впечатлений.";
         }
     }
 }
